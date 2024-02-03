889cb6786bc7be77c26602e7296ec685b7625af2
staman1702 <>
2024-02-03 14:17:49 +0000

    update secret_key

--- pp4_project/settings.py
+++ pp4_project/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ktvcp=ez@b-bfvk#wzhgwt-42+!2oe^c1=85*osq!%$2y33w-'
+# SECRET_KEY = 'django-insecure-8ktvcp=ez@b-bfvk#wzhgwt-42+!2oe^c1=85*osq!%$2y33w-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
