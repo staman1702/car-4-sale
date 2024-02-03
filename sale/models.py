8e06bdea304739dbc801c24f0f681f343edae150
staman1702 <>
2024-02-03 14:55:50 +0000

    adds a Sale, CarModel & CarMake models

--- sale/models.py
+++ sale/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.make} - {self.name}"
+
+
+class Sale(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sale_posts"
+    )
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
