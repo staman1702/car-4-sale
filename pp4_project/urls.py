816359241b69c726feb89e0981b27ed1fd038b4b
staman1702 <>
2024-02-03 15:58:06 +0000

    generic post view

--- pp4_project/urls.py
+++ pp4_project/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from sale.views import my_sale
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sale/', my_sale, name='sale'),
+    path("", include("sale.urls"), name="sale-urls"),
 ]
