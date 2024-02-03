4c8888817d25fe72809d3bc9de8b23a47f008839
staman1702 <>
2024-02-03 15:21:30 +0000

    debug false commit

--- pp4_project/settings.py
+++ pp4_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-staman1702-car-4-sale-nymsgjhim4.us2.codeanyapp.com', '.herokuapp.com']
