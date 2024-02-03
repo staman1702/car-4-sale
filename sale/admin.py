f7bc57490e2b864e6673893d31ec77429a83203e
staman1702 <>
2024-02-03 19:20:14 +0000

    Install summernote

--- sale/admin.py
+++ sale/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Post, CarMake, CarModel, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register()
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(CarMake)
 admin.site.register(CarModel)
 admin.site.register(Comment)
