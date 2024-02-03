2543d5ca0a42898d7a4895e307bc7b62fc9813ed
staman1702 <>
2024-02-03 15:30:35 +0000

    Add Comment model

--- sale/admin.py
+++ sale/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, CarMake, CarModel
+from .models import Post, CarMake, CarModel, Comment
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(CarMake)
 admin.site.register(CarModel)
+admin.site.register(Comment)
