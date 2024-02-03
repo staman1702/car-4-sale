08cf4797e617af28266119eaf6c7cfbc74ba6a16
staman1702 <>
2024-02-03 15:18:04 +0000

    update post model with excerpt & updated_on fields

--- sale/models.py
+++ sale/models.py
@@ -33,6 +33,8 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
