1e41afba02eb98dddd6d83fa032dfec15b8fad32
staman1702 <>
2024-02-04 16:50:08 +0000

    added post_detail view

--- sale/views.py
+++ sale/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+# from django.contrib import messages
 from .models import Post
+# from .forms import CommentForm
 
 # Create your views here.
 
@@ -10,3 +12,22 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "sale/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`sale.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`sale.Post`.
+
+    **Template:**
+
+    :template:`sale/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "sale/post_detail.html", {"post": post},)
