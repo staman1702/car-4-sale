3a5fd001c58809ba83621a8e78b48be19e86e345
staman1702 <>
2024-02-07 22:44:02 +0000

    posting to db solution code

--- sale/forms.py
+++ sale/forms.py
@@ -1,1 +1,9 @@
 # forms.py
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- sale/views.py
+++ sale/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-# from django.contrib import messages
+from django.contrib import messages
 from .models import Post
-# from .forms import CommentForm
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -30,4 +30,30 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "sale/post_detail.html", {"post": post},)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "sale/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
