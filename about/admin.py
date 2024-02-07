425b1c5ecf56a5721439251c6f1a0453b4b3022c
staman1702 <>
2024-02-07 23:40:47 +0000

    Add collaboration form solution code

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,3 +7,9 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
