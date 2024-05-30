7e8e98543edd22a55b8203c22060ecd8e3ff07d2
staman1702 <staman1702@gmail.com>
2024-05-30 22:43:21 +0100

    Updated settings.py to use env.py

--- pp4_project/settings.py
+++ pp4_project/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 from decouple import config
 import os
 import dj_database_url
-# if os.path.isfile('env.py'):
-#    import env
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-8ktvcp=ez@b-bfvk#wzhgwt-42+!2oe^c1=85*osq!%$2y33w-'
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com', '127.0.0.1', 'localhost']
