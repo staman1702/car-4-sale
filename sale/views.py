02cb5e3e6662262a0539970509c8225d43d95674
staman1702 <staman1702@gmail.com>
2024-06-05 23:40:08 +0100

    Remove unused local variable from sale/views.py delete comment

--- sale/views.py
+++ sale/views.py
@@ -102,8 +102,6 @@ def comment_delete(request, slug, comment_id):
     """
     view to delete comment
     """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user or request.user.is_superuser:
